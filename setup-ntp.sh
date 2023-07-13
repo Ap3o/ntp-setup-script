4311c1423b9f24a8e63b95ed07179970b55fb3a5
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2023-07-13 16:32:51 +0500

    changing ntp server

--- setup-ntp.sh
+++ setup-ntp.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y ntp
 
 # Обновить конфигурацию сервера
-echo "server neod.space iburst" | sudo tee /etc/ntp.conf
+echo "server 135.181.90.9 iburst" | sudo tee /etc/ntp.conf
 
 # Настроить часовой пояс
 sudo timedatectl set-timezone Asia/Yekaterinburg
