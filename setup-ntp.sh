94245f43ace40f5d217660e381d5cb830bb85051
Nikita Flesakov <45170829+Ap3o@users.noreply.github.com>
2024-04-11 16:06:31 +0500

    Update setup-ntp.sh

--- setup-ntp.sh
+++ setup-ntp.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y ntp
 
 # Обновить конфигурацию сервера
-echo "server 194.190.168.1 iburst" | sudo tee /etc/ntp.conf
+echo "server 0.ru.pool.ntp.org iburst" | sudo tee /etc/ntp.conf
 
 # Настроить часовой пояс
 sudo timedatectl set-timezone Asia/Yekaterinburg
